edbfdf9ed7ab83ccccef4e90aaad73d622326f4c
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-11-21 09:19:44 -0600

    test.js trying to run this file, with node.

--- server/test.js
+++ server/test.js
@@ -1,20 +1,23 @@
 
 const axios = require('axios');
 
-async function getNumberOfFollowers() {
+async function testFunction() {
     const config = {
         headers: { 'Content-Type': 'application/json' },
         withCredentials: true,
       };
- //let res = await axios.get('https://api.github.com/users/janbodnar');
+ 
   let res = await axios.get('/messages/test');
   console.log(res);
+
+}
+
+testFunction();
+
 ///messages
+//let res = await axios.get('https://api.github.com/users/janbodnar');
 //   let nOfFollowers = res.data.followers;
 //   let location = res.data.location;
 
 //   console.log(`# of followers: ${nOfFollowers}`)
 //   console.log(`Location: ${location}`)
-}
-
-getNumberOfFollowers();
