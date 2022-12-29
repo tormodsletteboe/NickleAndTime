0ee6b1f36947d541af34dd27ae11991ae5379401
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-12-29 10:12:43 -0600

    small stuff

--- src/components/GoogleMap/GoogleMapNickleAndTime.jsx
+++ src/components/GoogleMap/GoogleMapNickleAndTime.jsx
@@ -203,7 +203,7 @@ function GoogleMapNickleAndTime() {
       marker.current.setMap(null);
       infowindow=null;
       //trigger a re render of autocomplete, im a terrible coder for this hahahaha
-      //TODO:This is where I am
+      //TODO:This is where I am 22
       setAddress("");
       setBusinessName("");
       return;
